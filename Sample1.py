beae0f063e10c65048439b2bfad61acfda8a5bce
Parvez <parvezsha.g@gmail.com>
2023-06-18 11:32:17 +0530

    Reverse digit2

--- Sample1.py
+++ Sample1.py
@@ -2,7 +2,16 @@ number = int(input("Enter any 2 digit number"))
 reversenumber = 0
 while number != 0:
     digit = number % 10
+    second = reversenumber
     reversenumber = reversenumber * 10 + digit
     number //= 10
 #subract greater number with smaller number
-print(reversenumber)    
+if (second > digit):
+    sub = second - digit
+else:
+    sub = digit -second
+result =float(sub /9)
+print("Reserved Digit :", reversenumber)   
+print("Subraction Diffirence is :", sub) 
+print("Difference Subracted by 9 :", result)
+ 
